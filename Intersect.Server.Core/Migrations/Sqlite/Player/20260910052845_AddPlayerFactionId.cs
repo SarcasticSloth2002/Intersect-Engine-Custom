e7b1972fecb0a03771f21dba72cb42b1d5d5ef54
SnarkySloth <slothmanprophecy@duck.com>
2026-09-09 22:29:53 -0700

    cscsdc
    csdcsc

---
+++ Intersect.Server.Core/Migrations/Sqlite/Player/20260910052845_AddPlayerFactionId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intersect.Server.Migrations.Sqlite.Player
+{
+    /// <inheritdoc />
+    public partial class AddPlayerFactionId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "FactionId",
+                table: "Players",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FactionId",
+                table: "Players");
+        }
+    }
+}
